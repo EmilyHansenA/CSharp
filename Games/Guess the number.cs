33c8f9a9a91b76c3d4623e677013be33c6ad411e
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2024-04-02 03:54:44 +0400

    Create Guess the number.cs

---
+++ Games/Guess the number.cs
@@ -0,0 +1,32 @@
+﻿int number;
+int lower, higher;
+int triesCount = 5;
+
+Random random = new Random();
+
+number = random.Next(0, 101);
+lower = random.Next(number - 10, number);
+higher = random.Next(number + 1, number + 10);
+
+Console.WriteLine($"Число от 0 до 100, больше чем {lower}, " +
+    $"меньше чем {higher}");
+Console.WriteLine($"Отгадайте число. У вас {triesCount} попыток.");
+
+while (triesCount-- > 0)
+{
+    Console.Write($"Догадка: ");
+
+    if (Convert.ToInt32(Console.ReadLine()) == number)
+    {
+        Console.WriteLine($"Вы выиграли. Число {number}");
+        break;
+    }
+    else if (triesCount != 0)
+    {
+        Console.WriteLine("Нет. Осталось попыток " + triesCount);
+    }
+    else
+    {
+        Console.WriteLine("Вы проиграли. Загаданное число: " + number);
+    }
+}
