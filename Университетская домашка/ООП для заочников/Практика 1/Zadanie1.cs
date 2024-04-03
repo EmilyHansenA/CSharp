9aad48468ebbfe0be4702c25a9483ad41484a7db
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2024-04-04 01:50:26 +0400

    Update Zadanie1.cs
    Изменила кодировку на UTF-8

--- Университетская домашка/ООП для заочников/Практика 1/Zadanie1.cs
+++ Университетская домашка/ООП для заочников/Практика 1/Zadanie1.cs
@@ -1,26 +1,26 @@
-using System;
+п»їusing System;
 
 class User
 {
     public string userName { get; set; }
     public int userAge { get; set; }
 
-    public User() //Конструктор без параметров.
+    public User() //РљРѕРЅСЃС‚СЂСѓРєС‚РѕСЂ Р±РµР· РїР°СЂР°РјРµС‚СЂРѕРІ.
     {
     }
 
-    public User(string name, int age) //Контструктор с параметрами.
+    public User(string name, int age) //РљРѕРЅС‚СЃС‚СЂСѓРєС‚РѕСЂ СЃ РїР°СЂР°РјРµС‚СЂР°РјРё.
     {
         userName = name;
         userAge = age;
     }
 
-    public void DisplayUserName() //Метод 1. Показывает полное имя User.
+    public void DisplayUserName() //РњРµС‚РѕРґ 1. РџРѕРєР°Р·С‹РІР°РµС‚ РїРѕР»РЅРѕРµ РёРјСЏ User.
     {
         Console.WriteLine($"User full name: {userName}.");
     }
 
-    public void DisplayUserAge() //Метод 2 на выбор. Показывает возраст User.
+    public void DisplayUserAge() //РњРµС‚РѕРґ 2 РЅР° РІС‹Р±РѕСЂ. РџРѕРєР°Р·С‹РІР°РµС‚ РІРѕР·СЂР°СЃС‚ User.
     {
         Console.WriteLine($"User age: {userAge}.");
     }
@@ -30,18 +30,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Enter user full name: "); //Сохраняем ввод User Name в переменную string userName.
+        Console.Write("Enter user full name: "); //РЎРѕС…СЂР°РЅСЏРµРј РІРІРѕРґ User Name РІ РїРµСЂРµРјРµРЅРЅСѓСЋ string userName.
         string userName = Console.ReadLine();
 
-        Console.Write("Enter user age: "); //Сохраняем ввод User Age в переменную int userAge + конвентируем из строки в int переменную для записи.
+        Console.Write("Enter user age: "); //РЎРѕС…СЂР°РЅСЏРµРј РІРІРѕРґ User Age РІ РїРµСЂРµРјРµРЅРЅСѓСЋ int userAge + РєРѕРЅРІРµРЅС‚РёСЂСѓРµРј РёР· СЃС‚СЂРѕРєРё РІ int РїРµСЂРµРјРµРЅРЅСѓСЋ РґР»СЏ Р·Р°РїРёСЃРё.
         int userAge = Convert.ToInt32(Console.ReadLine());
 
-        User user1 = new User(userName, userAge); //Создаем экземляр класса с переменными, которые мы получили при вводе.
-        user1.DisplayUserName(); //Вызов метода из класса User, который показывает userName
-        user1.DisplayUserAge(); //Вызов метода из класса User, который показывает userAge
+        User user1 = new User(userName, userAge); //РЎРѕР·РґР°РµРј СЌРєР·РµРјР»СЏСЂ РєР»Р°СЃСЃР° СЃ РїРµСЂРµРјРµРЅРЅС‹РјРё, РєРѕС‚РѕСЂС‹Рµ РјС‹ РїРѕР»СѓС‡РёР»Рё РїСЂРё РІРІРѕРґРµ.
+        user1.DisplayUserName(); //Р’С‹Р·РѕРІ РјРµС‚РѕРґР° РёР· РєР»Р°СЃСЃР° User, РєРѕС‚РѕСЂС‹Р№ РїРѕРєР°Р·С‹РІР°РµС‚ userName
+        user1.DisplayUserAge(); //Р’С‹Р·РѕРІ РјРµС‚РѕРґР° РёР· РєР»Р°СЃСЃР° User, РєРѕС‚РѕСЂС‹Р№ РїРѕРєР°Р·С‹РІР°РµС‚ userAge
 
-        User user2 = new User("Full User Name", 24); //Создаем экземляр класса с инициализацией в программе.
-        user2.DisplayUserName(); //Вызов метода из класса User, который показывает userName
-        user2.DisplayUserAge(); //Вызов метода из класса User, который показывает userAge
+        User user2 = new User("Full User Name", 24); //РЎРѕР·РґР°РµРј СЌРєР·РµРјР»СЏСЂ РєР»Р°СЃСЃР° СЃ РёРЅРёС†РёР°Р»РёР·Р°С†РёРµР№ РІ РїСЂРѕРіСЂР°РјРјРµ.
+        user2.DisplayUserName(); //Р’С‹Р·РѕРІ РјРµС‚РѕРґР° РёР· РєР»Р°СЃСЃР° User, РєРѕС‚РѕСЂС‹Р№ РїРѕРєР°Р·С‹РІР°РµС‚ userName
+        user2.DisplayUserAge(); //Р’С‹Р·РѕРІ РјРµС‚РѕРґР° РёР· РєР»Р°СЃСЃР° User, РєРѕС‚РѕСЂС‹Р№ РїРѕРєР°Р·С‹РІР°РµС‚ userAge
     }
 }
