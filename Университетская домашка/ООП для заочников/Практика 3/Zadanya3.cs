c5939b52a15fbb24380468d6281c3851baddbc56
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2024-04-05 03:48:31 +0400

    Update Zadanya3.cs
    Добавил комментарии.

--- Университетская домашка/ООП для заочников/Практика 3/Zadanya3.cs
+++ Университетская домашка/ООП для заочников/Практика 3/Zadanya3.cs
@@ -1,29 +1,33 @@
-﻿interface IBusiness
+﻿//Создаем интерфейс IBusiness и метод DoBusiness
+interface IBusiness
 {
     void DoBusiness();
 }
-
+//Создаем интерфейс ITech и метод WriteCode
 interface ITech
 {
     void WriteCode();
 }
-
+//Создали класс SoftwareDeveloper, который наследует 2 интерфейса IBusiness & ITech
 public class SoftwareDeveloper : IBusiness, ITech
 {
+    //Реализовали метод из интерфейса IBusiness
     public void DoBusiness()
     {
         Console.WriteLine("Doing business");
     }
-
+    //Реализовали метод из интерфейса ITech
     public void WriteCode()
     {
         Console.WriteLine("Writing code");
     }
 }
+//Основное тело программы
 class Program
 {
     static void Main(string[] args)
     {
+        //Создали экземляр класса SoftwareDeveloper и вызвали его методы
         SoftwareDeveloper softwareDeveloper = new SoftwareDeveloper();
         softwareDeveloper.DoBusiness();
         softwareDeveloper.WriteCode();
