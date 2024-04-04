857619e12a53c23fcd47dbbe233310d3fcda9baf
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2024-04-05 03:37:03 +0400

    Create Zadanya3.cs
    Задача 3.
    
    Создайте интерфейсы "IBusiness" и "ITech" с методами "DoBusiness()" и "WriteCode()", соответственно.
    Затем создайте класс "SoftwareDeveloper", который наследуется от обоих интерфейсов и реализует их методы.

---
+++ Университетская домашка/ООП для заочников/Практика 3/Zadanya3.cs
@@ -0,0 +1,31 @@
+﻿interface IBusiness
+{
+    void DoBusiness();
+}
+
+interface ITech
+{
+    void WriteCode();
+}
+
+public class SoftwareDeveloper : IBusiness, ITech
+{
+    public void DoBusiness()
+    {
+        Console.WriteLine("Doing business");
+    }
+
+    public void WriteCode()
+    {
+        Console.WriteLine("Writing code");
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        SoftwareDeveloper softwareDeveloper = new SoftwareDeveloper();
+        softwareDeveloper.DoBusiness();
+        softwareDeveloper.WriteCode();
+    }
+}
