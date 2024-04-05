cc330d47295b447777df0a6980ebd3f9f03a6844
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2024-04-05 04:46:22 +0400

    Create Zadanya4.cs
    Задача 4.
    
    Создайте интерфейсы "IVehicle" и "IDriver" с методами "Move()" и "Control()", соответственно.
    Затем создайте класс "Car" и "Truck" с множественным наследованием от указанных интерфейсов.
    Создайте дополнительные интерфейсы "IEngine" и "ILoadable" с методами "StartEngine()" и "LoadCargo()", и реализуйте их в классе "Truck".
    Далее создайте класс "TruckDriver", который реализует интерфейс "IDriver" и имеет доступ к управлению как грузовиком, так и легковым автомобилем.

---
+++ Университетская домашка/ООП для заочников/Практика 3/Zadanya4.cs
@@ -0,0 +1,81 @@
+﻿interface IVehicle
+{
+    void Move();
+}
+interface IDrive
+{
+    void Control();
+}
+interface IEngine
+{
+    void StartEngine();
+}
+interface ILoadable
+{
+    void LoadCargo();
+}
+
+class Car : IVehicle, IDrive
+{
+    public void Move()
+    {
+        Console.WriteLine("Машина едет");
+    }
+    public void Control()
+    {
+        Console.WriteLine("Водитель контролирует машину");
+    }
+
+}
+class Truck : IVehicle, IDrive, IEngine, ILoadable
+{
+    public void LoadCargo()
+    {
+        Console.WriteLine("Груз был загружен в грузовик");
+    }
+    public void StartEngine()
+    {
+        Console.WriteLine("Двигатель грузовика запустился");
+    }
+    public void Move()
+    {
+        Console.WriteLine("Грузовик едет");
+    }
+    public void Control()
+    {
+        Console.WriteLine("Водитель контролирует грузовик");
+    }
+}
+
+class TruckDriver : IDrive
+{
+    private Car car;
+    private Truck truck;
+
+    public TruckDriver()
+    {
+        car = new Car();
+        truck = new Truck();
+    }
+
+    public void Control()
+    {
+        car.Move();
+        car.Control();
+
+        truck.LoadCargo();
+        truck.StartEngine();
+        truck.Move();
+        truck.Control();
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        TruckDriver truckDriver = new TruckDriver();
+        truckDriver.Control();
+    }
+}
