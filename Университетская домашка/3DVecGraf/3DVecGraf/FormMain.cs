5fb77fca240da6e9a58ed8a16f5137e4d61e2de8
Lyaysan Gimatdinova <Lyaysan78@gmail.com>
2025-10-13 12:51:13 +0300

    MVP домашки по графике

---
+++ Университетская домашка/3DVecGraf/3DVecGraf/FormMain.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _3DVecGraf
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            this.Paint += FormMain_Paint;
+        }
+        Graphics g;
+        public void Draw()
+        {
+            g = CreateGraphics();
+            g.Clear(Color.White);
+            g.DrawEllipse(Pens.Black, 200, 150, 100, 100);
+            g.Dispose();
+        }
+        private void FormMain_Paint(object sender, PaintEventArgs e)
+        {
+            Draw();
+        }
+    }
+}
